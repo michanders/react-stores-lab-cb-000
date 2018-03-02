64bedd6ef33354f063318cd665ca3dfd3ce3d908
Michael Anderson <michanders@msn.com>
2018-03-02 00:09:16 +0000

    automatically backed up by learn

--- components/App.js
+++ components/App.js
@@ -4,9 +4,24 @@ class App extends React.Component {
   constructor (props) {
     super(props);
     this.state = {
-      // Your implementation here.
-    };
+      counter: counterStore.getState()
+   }
+   
+   this.handleDecrement = this.handleDecrement.bind(this);
+   this.handleIncrement = this.handleIncrement.bind(this);
   }
+  
+  handleIncrement(ev){
+    ev.preventDefault();
+    actions.increment();
+  }
+
+  handleDecrement(ev){
+    ev.preventDefault();
+    actions.decrement();
+  }
+
+  
   componentDidMount () {
     // Your implementation here.
   }
@@ -18,10 +33,10 @@ class App extends React.Component {
       <div className='app'>
         <h1 className='counter'></h1>
         <div className='actions'>
-          <button className='decrement'>
+          <button className='decrement' onClick={this.handleDecrement}>
             -
           </button>
-          <button className='increment'>
+          <button className='increment' onClick={this.handleIncrement}>
             +
           </button>
         </div>
