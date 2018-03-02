90da960c5efd5a58d89ddc1900533e7b8ffe095d
Michael Anderson <michanders@msn.com>
2018-03-02 00:16:53 +0000

    automatically backed up by learn

--- stores/counterStore.js
+++ stores/counterStore.js
@@ -1,8 +1,8 @@
-class CounterStore {
+import Store from './Store'
+
+export default class CounterStore extends Store {
   // Your implementation here.
   // Hint: extend the Store class!
 }
 
 const counterStore = new CounterStore();
-
-export default counterStore;
