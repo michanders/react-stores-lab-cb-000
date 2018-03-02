fede59fb21f2671164ff47ca0b8a9df89316dc98
Michael Anderson <michanders@msn.com>
2018-03-02 00:16:11 +0000

    automatically backed up by learn

--- stores/Store.js
+++ stores/Store.js
@@ -1,25 +1,25 @@
 export default class Store {
   constructor(initialState){
-      this.state = initialState;
-      this.listeners = [];
-    }
+    this.state = initialState;
+    this.listeners = [];
+  }
 
-    setState(state){
-      this.state = state;
-      for(const listener of this.listeners){
-        listener(state);
-      }
-    }
-
-    getState(){
-      return this.state;
-    }
-
-    addListener(listener){
-      this.listeners.push(listener);
-      const removeListener = () => {
-        this.listeners = this.listeners.filter((l) => listener !== l)
-      };
-      return removeListener;
+  setState(state){
+    this.state = state;
+    for(const listener of this.listeners){
+      listener(state);
     }
   }
+
+  getState(){
+    return this.state;
+  }
+
+  addListener(listener){
+    this.listeners.push(listener);
+    const removeListener = () => {
+      this.listeners = this.listeners.filter((l) => listener !== l)
+    };
+    return removeListener;
+  }
+}
