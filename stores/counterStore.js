383ffbcb13fda012ad68d005c95722e7b5624e5b
Michael Anderson <michanders@msn.com>
2018-03-02 00:17:17 +0000

    automatically backed up by learn

--- stores/counterStore.js
+++ stores/counterStore.js
@@ -7,18 +7,14 @@ export default class CounterStore extends Store {
  }
 
   increment(){
-
     const updatedState = this.getState() + 1;
     this.setState(updatedState);
-
   }
 
   decrement(){
     const updatedState = this.getState() - 1;
     this.setState(updatedState);
-
   }
-
 }
 
 const counterStore = new CounterStore();
