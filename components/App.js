b100efbb0ed5e182c7870d4ebaf39002491f3d6e
Michael Anderson <michanders@msn.com>
2018-03-02 00:35:08 +0000

    automatically backed up by learn

--- components/App.js
+++ components/App.js
@@ -11,7 +11,7 @@ class App extends React.Component {
     this.handleDecrement = actions.decrement.bind(this);
     this.handleIncrement = actions.increment.bind(this);
   }
-  
+
   handleIncrement(ev){
     ev.preventDefault();
     actions.increment();
